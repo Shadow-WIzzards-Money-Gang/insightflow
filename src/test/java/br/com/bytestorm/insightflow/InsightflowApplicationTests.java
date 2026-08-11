85e8cb7b2013a7747f80fac3480d36080de8056b
Antonio <antoniojacintoa@gmail.com>
2026-08-11 16:05:33 -0300

    feat: configurando o projeto

---
+++ src/test/java/br/com/bytestorm/insightflow/InsightflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bytestorm.insightflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsightflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
