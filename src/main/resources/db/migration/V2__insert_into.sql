5c2e1e2ed26733318dd01174614e5d12008ef0be
Antonio <antoniojacintoa@gmail.com>
2026-08-12 13:10:49 -0300

    feat: adicionando migration que adiciona dados

---
+++ src/main/resources/db/migration/V2__insert_into.sql
@@ -0,0 +1,50 @@
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('Protheus', 'ERP');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('RM', 'ERP');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('Datasul', 'ERP');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('Logix', 'ERP');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('RH Clock In', 'RH');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('RH Performance', 'RH');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('RD Station', 'Marketing e CRM');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('TOTVS Assinatura Eletronica', 'Assinatura');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('TOTVS Analytics', 'BI e Analytics');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('Fluig', 'Workflow e BPM');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('TOTVS Automacao Fiscal', 'Fiscal');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('TOTVS Educacional', 'Educacao');
+
+INSERT INTO produtos_totvs (nome, categoria) 
+VALUES ('Outros', 'Outros');
+
+INSERT INTO segmentos_clientes (nome) VALUES ('Saúde');
+INSERT INTO segmentos_clientes (nome) VALUES ('Distribuição');
+INSERT INTO segmentos_clientes (nome) VALUES ('Manufatura');
+INSERT INTO segmentos_clientes (nome) VALUES ('Serviços');
+INSERT INTO segmentos_clientes (nome) VALUES ('Contrução e Projetos');
+INSERT INTO segmentos_clientes (nome) VALUES ('Educacional');
+INSERT INTO segmentos_clientes (nome) VALUES ('Agroindústria');
+INSERT INTO segmentos_clientes (nome) VALUES ('Logistica');
+INSERT INTO segmentos_clientes (nome) VALUES ('Supermercados');
+INSERT INTO segmentos_clientes (nome) VALUES ('Varejo');
+INSERT INTO segmentos_clientes (nome) VALUES ('Outro');
